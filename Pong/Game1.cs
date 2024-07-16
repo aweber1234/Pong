8455eb090e391b436a82213d68a3341a589226df
aweber1234 <158605438+aweber1234@users.noreply.github.com>
2024-07-16 01:15:43 -0400

    Add project files.

---
+++ Pong/Game1.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Pong
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        Paddle paddle;
+        Paddle paddle2;
+        Ball ball;
+        SpriteFont font;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = Globals.WIDTH;
+            _graphics.PreferredBackBufferHeight = Globals.HEIGHT;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            paddle = new Paddle(false);
+            paddle2 = new Paddle(true);
+            ball = new Ball();
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            Globals.spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            Globals.pixel = new Texture2D(GraphicsDevice, 1, 1);
+            Globals.pixel.SetData<Color>(new Color[] { Color.White });
+            font = Content.Load<SpriteFont>("Score");
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            paddle.Update(gameTime);
+            paddle2.Update(gameTime);
+            ball.Update(gameTime, paddle, paddle2);
+            base.Update(gameTime);
+
+
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            // TODO: Add your drawing code here
+            Globals.spriteBatch.Begin();
+            Globals.spriteBatch.DrawString(font, Globals.player1_score.ToString(), new Vector2(100, 50), Color.White);
+            Globals.spriteBatch.DrawString(font, Globals.player2_score.ToString(), new Vector2(Globals.WIDTH - 112, 50), Color.White);
+            paddle.Draw(gameTime);
+            paddle2.Draw(gameTime);
+            ball.Draw();
+
+            Globals.spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Pong/Globals.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Pong
+{
+    internal class Globals
+    {
+        public static SpriteBatch spriteBatch;
+        public static int WIDTH = 640, HEIGHT = 480;
+        public static Texture2D pixel;
+        public static int player1_score, player2_score;
+
+
+    }
+}
